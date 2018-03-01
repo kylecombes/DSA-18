c1168f866f10032c8a7d84a3b5cbf267555a6111
Kyle Combes <kylecombes@gmail.com>
2018-02-28 21:48:07 -0500

    Finished day 3 of sorting

--- sorting/day03/src/CountingSort.java
+++ sorting/day03/src/CountingSort.java
@@ -2,12 +2,39 @@ public class CountingSort {
 
     /**
      * Use counting sort to sort positive integer array A.
-     * Runtime: TODO
+     * Runtime: O(N+k)
+     * Space: O(k)
      *
      * k: maximum element in array A
      */
     static void countingSort(int[] A) {
-        // TODO
+        if (A.length == 0) return;
+
+        // Find the max element (probably a bit faster this way than converting to a Collection and using Collections.max())
+        int k = A[0];
+        for (int i = 1; i < A.length; ++i) {
+            if (A[i] > k) k = A[i];
+        }
+
+        // Create an array of length k for keeping track of the count of each element
+        int[] buckets = new int[k+1];
+
+        // Count the occurrences of each element
+        for (int num : A) {
+            ++buckets[num];
+        }
+
+        // Build up the resulting (sorted) array
+        int resIdx = 0;
+        // For each number potentially in the array (i.e. a bucket)
+        for (int bucketsIdx = 0; bucketsIdx <= k; ++bucketsIdx) {
+            // For each occurrence of that number
+            for (int occurrenceIdx = 0; occurrenceIdx < buckets[bucketsIdx]; ++occurrenceIdx) {
+                // Save that number to the resulting array (A)
+                A[resIdx++] = bucketsIdx;
+            }
+        }
+
     }
 
 }
--- sorting/day03/src/Problems.java
+++ sorting/day03/src/Problems.java
@@ -1,7 +1,19 @@
+import java.util.LinkedList;
+
 public class Problems {
 
     static void sortNumsBetween100s(int[] A) {
-        // TODO
+        // Shift all numbers by 100 to get rid of the negatives temporarily
+        for (int i = 0; i < A.length; ++i) {
+            A[i] += 100;
+        }
+        // Sort the array using radix sort with a base of 2
+        RadixSort.radixSort(A, 2);
+
+        // Shift all numbers back to be in the range [-100, 100]
+        for (int i = 0; i < A.length; ++i) {
+            A[i] -= 100;
+        }
     }
 
     /**
@@ -19,14 +31,29 @@ public class Problems {
      * @param n The digit number (where 0 is the least significant digit)
      */
     static void countingSortByCharacter(String[] A, int n) {
-        // TODO
+        LinkedList<String>[] L = new LinkedList[26]; // 26 characters in the alphabet
+        for (int i  = 0; i < 26; ++i) {
+            L[i] = new LinkedList<String>();
+        }
+        // Group each string by their nth letter
+        for (String string : A) {
+            L[getNthCharacter(string, n)].push(string);
+        }
+        // Rebuild the array
+        int aIdx = 0;
+        for (LinkedList<String> stringList : L) {
+            for (int i = stringList.size()-1; i >= 0; --i) {
+                A[aIdx++] = stringList.get(i);
+            }
+        }
     }
 
     /**
      * @param stringLength The length of each of the strings in S
      */
     static void sortStrings(String[] S, int stringLength) {
-        // TODO
+        for (int i = 0; i < stringLength; ++i)
+            countingSortByCharacter(S, i);
     }
 
     /**

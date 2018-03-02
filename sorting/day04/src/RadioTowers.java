90679a6381bb122634b830637e8249ea0bd5a8d8
Kyle Combes <kylecombes@gmail.com>
2018-03-01 21:34:28 -0500

    Finished sorting day 4 (radio towers)

--- sorting/day04/src/RadioTowers.java
+++ sorting/day04/src/RadioTowers.java
@@ -23,14 +23,35 @@ public class RadioTowers {
     // Strip contains a list of Towers sorted by x-coordinate, whose y-coordinates all fall in a 2-mile strip
     // Return true if no two towers are within 1 mile
     public static boolean checkStrip(List<Tower> strip) {
-        // TODO
-        return false;
+        // Maybe need to sort -- but constant time because only sorting 8 elements
+        // Could break if you had a bunch of towers with the same y coordinate
+        for (int i = 0; i < strip.size(); ++i) {
+            for (int j = i+1; j < i+8 && j < strip.size(); ++j) {
+                if (isWithin(strip.get(i), strip.get(j), 1))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkSection(List<Tower> Ly, int lo, int hi) {
+        if (hi - lo < 9)
+            return checkStrip(Ly.subList(lo, hi));
+
+        int midIdx = (hi - lo) / 2 + lo;
+        // Check bottom half
+        if (!checkSection(Ly, lo, midIdx))
+            return false;
+        // Check top half
+        if (!checkSection(Ly, midIdx, hi))
+            return false;
+        // Check 8 points along boundary
+        return checkSection(Ly, midIdx - 4, midIdx + 4);
     }
 
     // Return true if no two towers are within distance 1 of each other
     public static boolean validTowers(List<Tower> Lx, List<Tower> Ly) {
         assert Lx.size() == Ly.size();
-        // TODO
-        return false;
+        return checkSection(Ly, 0, Ly.size());
     }
 }

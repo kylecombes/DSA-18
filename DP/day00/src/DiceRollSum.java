e257020d6f80e56c0d246f94db5d633d24cf00f9
Kyle Combes <kylecombes@gmail.com>
2018-04-24 11:28:21 -0400

    Finished DP day 00

--- DP/day00/src/DiceRollSum.java
+++ DP/day00/src/DiceRollSum.java
@@ -1,10 +1,26 @@
+import java.util.HashMap;
+
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    private static HashMap<Integer, Integer> memo = new HashMap<>();
+
+    // Runtime: O(N)
+    // Space: O(N)
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+        if (N < 0)
+            return 0;
+        if (N < 2)
+            return 1;
+
+        if (memo.containsKey(N))
+            return memo.get(N);
+
+        int total = 0;
+        for (short roll = 1; roll <= 6; ++roll) {
+            total += diceRollSum(N-roll);
+        }
+        memo.put(N, total);
+        return total;
     }
 
 }
--- DP/day00/src/LongestIncreasingSubsequence.java
+++ DP/day00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,33 @@
 public class LongestIncreasingSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(N^2)
+    // Space: O(N)
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        if (A.length == 0)
+            return 0;
+
+        // Initialize an array indicating the length of the LIS for each element in the original array
+        int[] lis = new int[A.length];
+        for (short i = 0; i < lis.length; ++i)
+            lis[i] = 1;
+
+        // Compute the LIS for each element in the array
+        for (short i = 1; i < A.length; ++i) {
+            // For each preceding element
+            for (short j = 0; j < i; ++j) {
+                // Check if the element is less than element i and part of a longer subsequence than currently recorded
+                if (A[i] > A[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+            }
+        }
+
+        // Find the global LIS
+        int maximum = 1;
+        for (int elemLIS : lis) {
+            if (maximum < elemLIS)
+                maximum = elemLIS;
+        }
+
+        return maximum;
     }
 }
